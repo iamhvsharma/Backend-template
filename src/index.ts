95fd64908549a674150a1b2ee4a02eac9b5c21d6
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-02-26 02:10:19 +0530

    Template for backend

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+require("dotenv").config()
+import { app } from "./app";
+import connectDB from "./db/db"
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERR:", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running!!!");
+    });
+  })
+  .catch((err) => {
+    console.log(`MONGODB connection Fails!!!`, err);
+  });
